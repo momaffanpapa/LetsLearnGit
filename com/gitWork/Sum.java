887662ede1d8923783208f991a6b25731c5e6251
anish <anish15java@gmail.com>
2024-07-31 23:21:31 +0530

    updated2 sum.java

--- com/gitWork/Sum.java
+++ com/gitWork/Sum.java
@@ -4,5 +4,6 @@ public class Sum {
     public static void main(String[] args) {
         System.out.println("Hello World");
          System.out.println("Java");
+        System.out.println(2+8);
     }
 }
